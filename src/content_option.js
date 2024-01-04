d33f99cddcfdc55484d88bd81b353f43725ef7b6
Meraj Alam <59961917+merajalamkhan@users.noreply.github.com>
2024-01-04 10:51:29 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,172 @@
+const logotext = "Meraj Pathaan";
+const meta = {
+    title: "Meraj Alam",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Meraj Alam",
+    animated: {
+        first: "I 'am React & Next-Js Developer",
+        second: "I 'am Firebase Engineer",
+        third: "I 'am Fullstack Developer",
+    },
+    description: "Hi.. My Self Meraj Alam and I am doing my MCA and I am Complete MERN Stack Course.",
+    your_img_url: "./heromak.png",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "My name is Meraj Alam i am doing MCA from Dr. APJ Abdul kalam technical University and I have completed MERN Stack course i have made many  projects related to Web Development and I am React Js Developer I need a internship in your company to further improve my skills.",
+};
+const worktimeline = [{
+        jobtitle: "MERN Stack Course",
+        where: "ZN Infotech Pvt. Ltd.",
+        date: "2022-23",
+    },
+    {
+        jobtitle: "JavaScript Developer",
+        where: "Citron Software Pvt. Ltd.",
+        date: "1-month in 2022",
+    },
+    {
+        jobtitle: "Master of Computer Application",
+        where: "AKTU",
+        date: "2021-23",
+    },
+];
+
+const skills = [{
+        name: "HTML5",
+        value: 99,
+    },
+    {
+        name: "CSS3",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "React-Js",
+        value: 98,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Bootstrap5",
+        value: 95,
+    },
+    {
+        name: "Tailwind CSS",
+        value: 75,
+    },
+    {
+        name: "Material UI",
+        value: 79,
+    },
+    {
+        name: "Firebase",
+        value: 85,
+    },
+    {
+        name: "Node-Js",
+        value: 95,
+    },
+    {
+        name: "Express-Js ",
+        value: 75,
+    },
+    {
+        name: "MongoDB",
+        value: 82,
+    },
+    {
+        name: "Canvas",
+        value: 99,
+    },
+    {
+        name: "MS-office",
+        value: 98,
+    },
+];
+
+const services = [{
+        title: "Static website",
+        description: "These website is a Static pages with Email Form (Rs. 4999 - 6999) with - Free Domail and Hosting .",
+    },
+    {
+        title: "Dynamic website",
+        description: "These website are Dynamic work for any Department (Rs. 5999 - 9999) with - Free Domail and Hosting .",
+    },
+    {
+        title: "E-commerce website",
+        description: "The E-commerce website are fully Dynamic & Provide Admin Panel (Rs. 10500 - 14999) with - Free Domail and Hosting .",
+    },
+];
+
+const dataportfolio = [{
+        img: "./back1.jpeg",
+        description: "1.These is Static Website for E-Commerce to Show the all infrmation on the Internet Server.",
+        link: "http://thecomputer.onlinewebshop.net/",
+    },
+    {
+        img: "./back2.jpeg",
+        description: "2.These is Static Website for the Customer Portfolio.",
+        link: "http://himanshi.myartsonline.com/",
+    },
+    {
+        img: "./back3.jpeg",
+        description: "3.These is Static Website for Usmania Hospital to Show the all infrmation on the Server.",
+        link: "https://usmaniahospital.netlify.app/",
+    },
+    {
+        img: "./back4.jpg",
+        description: "4.These Project is Software Company and Institute Website to Show the all information on the Server.",
+        link: "https://zninfotech.netlify.app/",
+    },
+    {
+        img: "./back5.png",
+        description: "4.These Project is crete to Customer Portfoio to Show the all Deatials on the Server.",
+        link: "https://priyanjali.netlify.app/",
+    },
+    {
+        img: "./back6.png",
+        description: "4.These Project is Learning Purpose to Create CURD Operations.",
+        link: "https://reactcurd.netlify.app/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "merajalam7458@gmail.com",
+    YOUR_FONE: "(+91) 7458855961",
+    description: "Hi.. I am Fullstack web Developer using React-Js, Next-Js and Firebase to Genrate a Website for the Customers and Companys. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_re96duz",
+    YOUR_TEMPLATE_ID: "template_5mupmbv",
+    YOUR_USER_ID: "daDWM8pIJqJJ0enrq",
+};
+
+const socialprofils = {
+    github: "https://github.com/merajalamkhan",
+    facebook: "https://www.facebook.com/merajalam9211",
+    linkedin: "https://www.linkedin.com/in/meraj-pathaan-7a7a041a2/",
+    whatsapp: "https://wa.link/vu5mwm",
+    instagram: "https://www.instagram.com/black_rider_840/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
